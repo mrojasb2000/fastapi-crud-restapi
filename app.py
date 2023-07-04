7d7f7a3d9abca81ff2bcab0df9ab6604ccd238b1
mauricio rojas <mrojasb2000@gmail.com>
2023-07-04 11:15:03 -0400

    chore: add base model

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("Welcome Python Anaconda version 3.9.16")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Text, Optional
+from datetime import datetime
+
+app = FastAPI()
+posts = []
+
+# Post model
+class Post(BaseModel):
+    id: Optional[str]
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: Optional[datetime]
+    published: bool = False
+
+@app.get("/")
+def read_root():
+    return {"welcome": "welcome to my REST API"}
+
+@app.get("/posts")
+def getPosts():
+    return posts
+
+@app.post("/posts")
+def create(post):
+    print(post)
